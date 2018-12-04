c81a3c70606f9f4e9eefd60eb8c69c5783f6aba6
malklkathi <malklkathi>
2018-12-03 19:59:06 -0500

    python

---
+++ start.sh
@@ -0,0 +1,8 @@
+export FLASK_APP=microblog.py
+export FLASK_DEBUG=1
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME="webprogramming991"
+export MAIL_PASSWORD="Qazxsw12@"
+flask run
